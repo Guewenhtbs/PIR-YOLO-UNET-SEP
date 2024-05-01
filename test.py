dc27b103dbdb421c578c091a35149a1c6f6fc5f9
Guewenhtbs <kerguegue@gmail.com>
2024-05-01 17:34:34 +0200

    slicing a tester

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import numpy as np
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 
-# A path to a T1-weighted brain .nii image:
 flair = r'c:\Users\kergu.LAPTOP-RGB94A60\Documents\TC\PIR\Brain MRI Dataset of Multiple Sclerosis with Consensus Manual Lesion Segmentation and Patient Meta Information\Patient-1\1-Flair.nii'
 
 flair_seg = r'c:\Users\kergu.LAPTOP-RGB94A60\Documents\TC\PIR\Brain MRI Dataset of Multiple Sclerosis with Consensus Manual Lesion Segmentation and Patient Meta Information\Patient-1\1-LesionSeg-Flair.nii'
@@ -13,15 +13,15 @@ sitk_flair = sitk.ReadImage(flair)
 sitk_flair_seg = sitk.ReadImage(flair_seg)
 
 # and access the numpy array:
-ar_flair = sitk.GetArrayFromImage(sitk_flair)
-ar_flair_seg = sitk.GetArrayFromImage(sitk_flair_seg)
+ar_flair = sitk.GetArrayFromImage(sitk_flair, type=np.float32)
+ar_flair_seg = sitk.GetArrayFromImage(sitk_flair_seg, type=np.float32)
 
 normalizer = sitk.NormalizeImageFilter()
 
 n_flair = normalizer.Execute(sitk_flair)
 
-ar_n_flair = sitk.GetArrayFromImage(n_flair)
-
+ar_n_flair = sitk.GetArrayFromImage(n_flair, type=np.float32)
+"""
 fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
 axes[0].imshow(ar_flair[9,:,:], cmap='gray')
@@ -33,8 +33,14 @@ axes[1].set_title('Normalized Flair')
 axes[2].imshow(ar_flair_seg[9,:,:], cmap='gray')
 axes[2].set_title('Flair Segmentation')
 
-plt.show()
-
+plt.show()"""
+X=[]
+y=[]
 for i in range(23) :
     if ar_flair_seg[i,:,:].max() > 0 :
-        print(i)
+        X.append(ar_n_flair[i,:,:])
+        y.append(ar_flair_seg[i,:,:])
+X = np.asarray(X, dtype=np.float32)
+X = np.expand_dims(X,-1)
+y = np.asarray(y, dtype=np.float32)
+y = keras.utils.to_categorical(y)
